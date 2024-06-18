d0a364384832c6370ac6c2f28b44698feccdb623
David Nazariego Ulacio <dnazariego@gmail.com>
2024-06-18 17:55:08 +0000

    traffic

--- src/js/component/light.jsx
+++ src/js/component/light.jsx
@@ -9,8 +9,8 @@ const TrafficLight = () => {
                 <div className="base"></div>
                 <div className="traffic-light">
                     <div onClick={() => setColor(color === 'red' ? "" : 'red')} className={"light red" + ((color === "red") ? " glow" : "")}></div>
-                    <div onClick={() => setColor('yellow')} className={"light yellow" + ((color === "yellow") ? " glow" : "")}></div>
-                    <div onClick={() => setColor('green')} className={"light green" + ((color === "green") ? " glow" : "")}></div>
+                    <div onClick={() => setColor(color === 'yellow' ? "" : 'yellow')} className={"light yellow" + ((color === "yellow") ? " glow" : "")}></div>
+                    <div onClick={() => setColor(color === 'green' ? "" : 'green')} className={"light green" + ((color === "green") ? " glow" : "")}></div>
                 </div>
             </div>
         </div>
